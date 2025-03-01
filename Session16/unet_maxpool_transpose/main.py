70fc76776984cd55e28adcf41d40857d0b5cd6ad
kishkath <kiranchw000@gmail.com>
2025-03-01 12:20:46 +0530

    restructred the directory with proper files

--- Session16/unet_maxpool_transpose/main.py
+++ Session16/unet_maxpool_transpose/main.py
@@ -4,6 +4,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import torch
 import torch.optim as optim
